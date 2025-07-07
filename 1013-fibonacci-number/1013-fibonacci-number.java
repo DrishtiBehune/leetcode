8dd926770343cc65608659c5e9a6a194ef72a572
DrishtiBehune <behunedp@rknec.edu>
2025-07-07 13:02:35 +0530

    Time: 0 ms (100%), Space: 40.1 MB (90.91%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int fib(int n) {
+        int i = 0 ;
+        int sum = 0;
+             if(n==0){
+                return 0;
+            }
+            if(n==1){
+                return 1;
+            }
+            
+        int [] arr = new int[n+1];
+         arr[0] = 0;
+         arr[1] = 1;
+      
+        for(i=2;i<=n;i++){
+           arr[i] = arr[i-2] + arr[i-1] ;
+        }
+        
+           
+           return arr[n];
+    }
+}
