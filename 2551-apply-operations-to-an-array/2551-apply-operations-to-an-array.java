ea75592cb96dbd19db0eaa38487040f3fcde665c
DrishtiBehune <behunedp@rknec.edu>
2025-07-08 15:16:47 +0530

    Time: 1 ms (92.73%), Space: 43.5 MB (93.06%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
@@ -1,20 +1,24 @@
 class Solution {
     public int[] applyOperations(int[] nums) {
-        
+        int k = 0 ;
         int n = nums.length;
         int i;
         int l = 0;
         for(i=0;i<n-1;i++){
            if(nums[i] == nums[i+1]){
-            nums[i] = nums[i] * 2;
-            nums[i+1] = 0 ;
-            }
-            
-           
+            nums[k] = nums[i] * 2;
+            nums[k+1] = 0 ;
            }
-           for(i=0;i<n;i++){
-            if(nums[i] != 0){
-             nums[l] = nums[i];
+           else{
+            nums[k] = nums[i];
+              }
+              k++ ;
+           }
+            k = 0;
+           for(k=0;k<n;k++){
+           
+            if(nums[k] != 0){
+             nums[l] = nums[k];
              l++;
             }
            }
@@ -26,3 +30,5 @@ class Solution {
          
     }
 }
+
+
