d0994b71060ce43eae47515bd7e4037e9677c4a3
DrishtiBehune <behunedp@rknec.edu>
2025-07-08 19:14:50 +0530

    Time: 1 ms (59.9%), Space: 45 MB (23.18%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -3,9 +3,9 @@ class Solution {
         int i;
         int k = 1;
         int n = nums.length;
-        for(i=0;i<n-1;i++){
-            if(nums[i] != nums[i+1]){
-                nums[k] = nums[i+1];
+        for(i=1;i<n;i++){
+            if(nums[i] != nums[i-1]){
+                nums[k] = nums[i];
                 k++ ;
             }
             
